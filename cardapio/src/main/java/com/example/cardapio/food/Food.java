caf72f946c7a50a4e08b3dc1f3b602a30c58d246
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-05-07 21:56:15 -0300

    gerando getters e setters para n dar ruim depois

--- cardapio/src/main/java/com/example/cardapio/food/Food.java
+++ cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -15,4 +15,39 @@ public class Food {
     private String image;
 
     private Integer price;
+
+    public Food() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
